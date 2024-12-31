f56430a25f5bb21972ed1d4a8d6953678f3d7333
mohamed shaaban <mohamedshaabab34@gmail.com>
2024-12-31 16:45:20 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+// console.log(col1);
+function phones(src) {
+    img.src = src;
+}
+function colors(color) {
+    container.style.background = color;
+}
