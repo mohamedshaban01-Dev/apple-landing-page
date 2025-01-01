24cedf767c090928558ae59b696715cac186307c
mohamed shaaban <mohamedshaabab34@gmail.com>
2025-01-01 10:40:30 +0200

    Fixed missing files and corrected paths

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let img = document.querySelector('.img');
 let container = document.querySelector('.container');
+let icon = document.querySelector(".icon");
+let menu = document.querySelector('.menu');
 
 // console.log(col1);
 function phones(src) {
@@ -8,3 +10,17 @@ function phones(src) {
 function colors(color) {
     container.style.background = color;
 }
+// function visiabilty(visible) {
+//     menu.style.visibility = visible;
+// }
+icon.addEventListener("click", (event) => {
+    menu.style.visibility = "visible";
+    event.stopPropagation(); 
+});
+
+// to hidden
+document.addEventListener("click", (event) => {
+    if (!menu.contains(event.target) && event.target !== icon) {
+        menu.style.visibility = "hidden";
+    }
+});
